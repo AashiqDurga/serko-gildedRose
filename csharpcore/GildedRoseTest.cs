cae25a32624a079ee1fd80c74d9d32e627d4b9e1
Aashiq Durga <aashiqdurga@gmail.com>
2019-08-02 16:40:09 +1200

    Add tests fro generic item

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -163,5 +163,65 @@ namespace csharpcore
             _gildedRose.UpdateQuality(items);
             Assert.Equal(80, item.Quality);
         }
+
+        [Fact]
+        public void GivenGenericItem_WhenUpdateQuality_QualityDecreasesBy1()
+        {
+            var item = new Item
+            {
+                Name = "Generic",
+                Quality = 45,
+                SellIn = 1
+            };
+            
+            var items = new List<Item> { item };
+            _gildedRose.UpdateQuality(items);
+            Assert.Equal(44, item.Quality);
+        }
+        
+        [Fact]
+        public void GivenGenericItem_WhenUpdateQuality_SellInDecreasesBy1()
+        {
+            var item = new Item
+            {
+                Name = "Generic",
+                Quality = 45,
+                SellIn = 1
+            };
+            
+            var items = new List<Item> { item };
+            _gildedRose.UpdateQuality(items);
+            Assert.Equal(0, item.SellIn);
+        }
+        
+        [Fact]
+        public void GivenGenericItemWithQuality0_WhenUpdateQuality_QualityRemainsZero()
+        {
+            var item = new Item
+            {
+                Name = "Generic",
+                Quality = 0,
+                SellIn = 1
+            };
+            
+            var items = new List<Item> { item };
+            _gildedRose.UpdateQuality(items);
+            Assert.Equal(0, item.Quality);
+        }
+        
+        [Fact]
+        public void GivenGenericItemWithSellInLessThan1_WhenUpdateQuality_QualityDecrementsBy2()
+        {
+            var item = new Item
+            {
+                Name = "Generic",
+                Quality = 10,
+                SellIn = 0
+            };
+            
+            var items = new List<Item> { item };
+            _gildedRose.UpdateQuality(items);
+            Assert.Equal(8, item.Quality);
+        }
     }
 }
