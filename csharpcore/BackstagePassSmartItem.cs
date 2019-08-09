42c1f60a97afe2b6bd89f8f6848c30efd125e250
Aashiq Durga <aashiqdurga@gmail.com>
2019-08-09 16:21:37 +1200

    Refactored implementation

---
+++ csharpcore/BackstagePassSmartItem.cs
@@ -0,0 +1,51 @@
+namespace csharpcore
+{
+    public class BackstagePassSmartItem : ISmartItem
+    {
+        public BackstagePassSmartItem(Item item)
+        {
+            Item = item;
+        }
+
+        public Item Item { get; set; }
+
+        public void Update()
+        {
+            HandleBackstagePass();
+        }
+        
+        private void HandleBackstagePass()
+        {
+            IncreaseQuality();
+
+            if (Item.SellIn <= 10)
+            {
+                IncreaseQuality();
+            }
+
+            if (Item.SellIn <= 5)
+            {
+                IncreaseQuality();
+            }
+
+            DecreaseSellIn();
+            if (Item.SellIn < 0)
+            {
+                Item.Quality = 0;
+            }
+        }
+        
+        private void IncreaseQuality()
+        {
+            if (Item.Quality < 50)
+            {
+                Item.Quality++;
+            }
+        }
+
+        private void DecreaseSellIn()
+        {
+            Item.SellIn--;
+        }
+    }
+}
