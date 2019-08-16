0e152d0e52b42ee0e40cc9bc667a699c70510492
Aashiq Durga <aashiqdurga@gmail.com>
2019-08-16 14:53:31 +1200

    Added smart item tests for other types

--- csharpcore/SmartItemFactory.cs
+++ csharpcore/SmartItemFactory.cs
@@ -3,13 +3,21 @@ namespace csharpcore
     public class SmartItemFactory
     {
         private const string AgedBrie = "Aged Brie";
+        private const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        private const string SulfurasHandOfRagnaros = "Sulfuras, Hand of Ragnaros";
 
         public ISmartItem Create(Item item)
         {
             if (item.Name == AgedBrie)
                 return new AgedBrieSmartItem(item);
+            
+            if (item.Name == BackstagePass)
+                return new BackstagePassSmartItem(item);
+            
+            if (item.Name == SulfurasHandOfRagnaros)
+                return new SulfurasSmartItem(item);
 
-            return null;
+            return new GenericSmartItem(item);
         }
     }
 }
--- csharpcore/SmartItemFactoryTests.cs
+++ csharpcore/SmartItemFactoryTests.cs
@@ -1,20 +1,29 @@
+using System;
 using Xunit;
 
 namespace csharpcore
 {
     public class SmartItemFactoryTests
     {
-        [Fact]
-        public void GivenItemWithNameAgedBrie_ReturnsAgedBrieSmartItem()
+        private const string AgedBrie = "Aged Brie";
+        private const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        private const string SulfurasHandOfRagnaros = "Sulfuras, Hand of Ragnaros";
+
+        [Theory]
+        [InlineData(AgedBrie, typeof(AgedBrieSmartItem))]
+        [InlineData(BackstagePass, typeof(BackstagePassSmartItem))]
+        [InlineData(SulfurasHandOfRagnaros, typeof(SulfurasSmartItem))]
+        [InlineData("generic-item", typeof(GenericSmartItem))]
+        public void GivenItemWithNameAgedBrie_ReturnsAgedBrieSmartItem(string itemName, Type expectedType)
         {
             var item = new Item
             {
-                Name = "Aged Brie"
+                Name = itemName
             };
             
             var smartItemFactory = new SmartItemFactory();
             var smartItem = smartItemFactory.Create(item);
-            Assert.IsType(typeof(AgedBrieSmartItem), smartItem);
+            Assert.IsType(expectedType, smartItem);
         }
     }
 }
